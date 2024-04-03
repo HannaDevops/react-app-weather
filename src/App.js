167e2df12b2fe40d5d2b2ca44e3bbb968d643ec8
HannaDevops <115883036+HannaDevops@users.noreply.github.com>
2024-04-03 22:01:57 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-       
+       <div className="cotainer">
+      
         <h1>Weather app</h1> 
      <footer>
       This project was coded by {" "}
@@ -12,7 +13,7 @@ export default function App() {
         <a href="https://github.com/HannaDevops/react-app-weather.git" rel="noreferrer" target="_blank">open-sourced</a> 
         
         </footer>  
-    </div>
+    </div></div>
   );
 }
 
